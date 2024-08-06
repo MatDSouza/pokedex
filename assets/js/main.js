4658ab30dbd23e66d17a5fcd3591f86a124c10c3
Mateus de Souza <masori.galo9@hotmail.com>
2024-08-06 19:15:34 -0300

    Atualizando API

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,36 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToli(pokemon) {
+    return ` 
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="details">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://media.tenor.com/XtXuPwRrjoIAAAAj/joia.gif" alt="${pokemon.name}">    
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody)=> jsonBody.results)
+    .then((pokemons) =>{ 
+      
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToli(pokemon)
+        }
+    })
     .catch((error) => console.error(error))
     
